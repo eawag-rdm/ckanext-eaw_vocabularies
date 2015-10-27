cac04967c07f5a6187704ac6a7940023c68719cf
Harald von Waldow <harald.vonwaldow@eawag.ch>
2015-10-27 17:17:30 +0100

    WIP / custom fields to into template

--- ckanext/eaw_vocabularies/plugin.py
+++ ckanext/eaw_vocabularies/plugin.py
@@ -11,14 +11,39 @@ class Eaw_VocabulariesPlugin(p.SingletonPlugin, tk.DefaultDatasetForm):
     # IConfigurer
     def update_config(self, config_):
         tk.add_template_directory(config_, 'templates')
-        tk.add_public_directory(config_, 'public')
+        #tk.add_public_directory(config_, 'public')
         #toolkit.add_resource('fanstatic', 'eaw_vocabularies')
 
     # IDatasetform
-    def create_package_schema(self):
-        schema = super(Eaw_VocabulariesPlugin, self).create_package_schema()
+    def _modify_package_schema(self, schema):
         schema.update({
             'System': [tk.get_validator('ignore_missing'),
-                       tk.get_converter('convert_to_extras')]
-            })
+                       tk.get_converter('convert_to_extras')]})
         return(schema)
+        
+    def create_package_schema(self):
+        schema = super(Eaw_VocabulariesPlugin, self).create_package_schema()
+        schema = self._modify_package_schema(schama)
+        return(schema)
+
+    def update_package_schema(self):
+        schema = super(Eaw_VocabulariesPlugin, self).create_package_schema()
+        schema = self._modify_package_schema(schama)
+        return(schema)
+
+    def show_package_schema(self):
+        schema = super(Eaw_VocabulariesPlugin, self).show_package_schema()
+        schema.update({
+            'System': [tk.get_converter('convert_from_extras'),
+                       tk.get_validator('ignore_missing')]})
+        return(schema)
+    
+    def is_fallback(self):
+        # Return True to register this plugin as the default handler for
+        # package types not handled by any other IDatasetForm plugin.
+        return True
+
+    def package_types(self):
+        # This plugin doesn't handle any special package types, it just
+        # registers itself as the default (above).
+        return []
