539fe432c202be308cfb6bf055533953812e7981
Harald von Waldow <harald.vonwaldow@eawag.ch>
2015-10-27 16:46:35 +0100

    first schema mod

--- ckanext/eaw_vocabularies/plugin.py
+++ ckanext/eaw_vocabularies/plugin.py
@@ -1,13 +1,24 @@
-import ckan.plugins as plugins
-import ckan.plugins.toolkit as toolkit
+import ckan.plugins as p
+import ckan.plugins.toolkit as tk
 
 
-class Eaw_VocabulariesPlugin(plugins.SingletonPlugin):
-    plugins.implements(plugins.IConfigurer)
 
+
+class Eaw_VocabulariesPlugin(p.SingletonPlugin, tk.DefaultDatasetForm):
+    p.implements(p.IConfigurer)
+    p.implements(p.IDatasetForm)
+    
     # IConfigurer
-
     def update_config(self, config_):
-        toolkit.add_template_directory(config_, 'templates')
-        toolkit.add_public_directory(config_, 'public')
-        toolkit.add_resource('fanstatic', 'eaw_vocabularies')
+        tk.add_template_directory(config_, 'templates')
+        tk.add_public_directory(config_, 'public')
+        #toolkit.add_resource('fanstatic', 'eaw_vocabularies')
+
+    # IDatasetform
+    def create_package_schema(self):
+        schema = super(Eaw_VocabulariesPlugin, self).create_package_schema()
+        schema.update({
+            'System': [tk.get_validator('ignore_missing'),
+                       tk.get_converter('convert_to_extras')]
+            })
+        return(schema)
