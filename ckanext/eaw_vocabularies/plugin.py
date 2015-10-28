644cfdf639315518622e5be92367bd6bdc860789
Harald von Waldow <harald.vonwaldow@eawag.ch>
2015-10-28 18:21:03 +0100

    finished system field edit and show

--- ckanext/eaw_vocabularies/plugin.py
+++ ckanext/eaw_vocabularies/plugin.py
@@ -1,12 +1,15 @@
 import ckan.plugins as p
 import ckan.plugins.toolkit as tk
 
-
+def eaw_vocabularies_systems():
+    tag_list = tk.get_action('tag_list')
+    return(tag_list(data_dict={'vocabulary_id': 'system'}))
 
 
 class Eaw_VocabulariesPlugin(p.SingletonPlugin, tk.DefaultDatasetForm):
     p.implements(p.IConfigurer)
     p.implements(p.IDatasetForm)
+    p.implements(p.ITemplateHelpers)
     
     # IConfigurer
     def update_config(self, config_):
@@ -17,24 +20,25 @@ class Eaw_VocabulariesPlugin(p.SingletonPlugin, tk.DefaultDatasetForm):
     # IDatasetform
     def _modify_package_schema(self, schema):
         schema.update({
-            'System': [tk.get_validator('ignore_missing'),
-                       tk.get_converter('convert_to_extras')]})
+            'system': [tk.get_validator('ignore_missing'),
+                       tk.get_converter('convert_to_tags')('system')]})
         return(schema)
         
     def create_package_schema(self):
         schema = super(Eaw_VocabulariesPlugin, self).create_package_schema()
-        schema = self._modify_package_schema(schama)
+        schema = self._modify_package_schema(schema)
         return(schema)
 
     def update_package_schema(self):
-        schema = super(Eaw_VocabulariesPlugin, self).create_package_schema()
-        schema = self._modify_package_schema(schama)
+        schema = super(Eaw_VocabulariesPlugin, self).update_package_schema()
+        schema = self._modify_package_schema(schema)
         return(schema)
 
     def show_package_schema(self):
         schema = super(Eaw_VocabulariesPlugin, self).show_package_schema()
+        schema['tags']['__extras'].append(tk.get_converter('free_tags_only'))
         schema.update({
-            'System': [tk.get_converter('convert_from_extras'),
+            'system': [tk.get_converter('convert_from_tags')('system'),
                        tk.get_validator('ignore_missing')]})
         return(schema)
     
@@ -47,3 +51,7 @@ class Eaw_VocabulariesPlugin(p.SingletonPlugin, tk.DefaultDatasetForm):
         # This plugin doesn't handle any special package types, it just
         # registers itself as the default (above).
         return []
+    
+    # ITemplateHelpers
+    def get_helpers(self):
+        return({'systems': eaw_vocabularies_systems})
