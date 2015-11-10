ee47c14462f47bf0c2e56dc977598d228a453423
Harald von Waldow <harald.vonwaldow@eawag.ch>
2015-11-10 17:26:20 +0100

    make OR work

--- ckanext/eaw_vocabularies/plugin.py
+++ ckanext/eaw_vocabularies/plugin.py
@@ -15,17 +15,41 @@ def eaw_getnow():
     ''' Current date in ISO 8601'''
     return(dt.date.today().isoformat())
 
-def search_params2dict(search_params):
-    '''Converts the search_params 'fq'-value
-    that passed to before_view() to a dict.'''
-    fq_dict = dict([e.split(':') for e in search_params['fq'].split()])
-    # extract special entries
-    myqdict = dict([(k, v) for k, v in fq_dict.iteritems()
-                    if k in CUSTOM_Q_SEARCHES])
-    return(myqdict)
-
-CUSTOM_Q_SEARCHES = ['vocab_variables']
-CUSTOM_OPS = ['op_' + field for field in CUSTOM_Q_SEARCHES]
+def mk_field_queries(search_params):
+    '''
+    Customizes the fq-search-string so that query-terms
+    referring to the same field (e.g. "example_field") are combined
+    with logic operator taken from the value of op_<field>,
+    e.g. "op_example_field". Default for this operator is "OR". op_<field>
+    is removed from the querystring.
+    '''
+    
+    fq_list = [e.split(':') for e in search_params['fq'].split()]
+    operator_fields = [x for x in fq_list if x[0].startswith('op_')]
+    # Assert only one operator per field
+    assert (len(operator_fields) == len(set([x[0] for x in operator_fields])))
+    operator_fields = dict(operator_fields)
+    uniq_fields =  set([x[0] for x in fq_list if not x[0].startswith('op_')])
+    querystring = ''
+    for f in uniq_fields:
+        try:
+            operator = operator_fields['op_' + f].strip('"')
+        except KeyError:
+            operator = "OR"
+        queryterms = [x[1] for x in fq_list if x[0] == f]
+        querystring += ' '+f+':('+(' '+operator+' ').join(queryterms)+')'
+        print
+        print("f: {}".format(f))
+        print("terms: {}".format(queryterms))
+        print("qstring: {}".format(querystring))
+    search_params['fq'] = querystring
+    return(search_params)
+            
+## Needs to be put in config eventually. List of fields of
+## custom searches and logical operator to apply among terms with
+## the same field-name
+CUSTOM_SEARCH_FIELDS = ['vocab_variables', 'vocab_system']
+CUSTOM_OPS = ['op_' + field for field in CUSTOM_SEARCH_FIELDS]
 
 class Eaw_VocabulariesPlugin(p.SingletonPlugin, tk.DefaultDatasetForm):
     p.implements(p.IConfigurer)
@@ -108,30 +132,8 @@ class Eaw_VocabulariesPlugin(p.SingletonPlugin, tk.DefaultDatasetForm):
         # the controller puts everything into the value of "fq". Extract
         # what we want to treat.
         print("INPUT search_params {}".format(search_params))
-        fq = search_params2dict(search_params)
-        print("EXTRACTED DICTIONARY {}".format(str(fq)))
-        # cust_keys = [k for k in fq.keys() if k in CUSTOM_Q_SEARCHES]
-        # cust_ops = [k for k in fq.keys() if k in CUSTOM_OPS]
-        # ## remove those from search_params' fq-field
-        # for k in cust_keys + cust_ops:
-        #     del fq[k]
-        # search_params['fq'] = fq
-        # ## add them to q
-        # q = search_params['q']
-        
-        
-        
-        
-        # sp_dict = eval(search_params)
-        # print(type(sp_dict))
-        # try:
-        #     fqstring = search_params['fq']
-        #     if fqstring == 'vocab_system:" "':
-        #         del search_params['fq']
-        #     else:
-        #         fqstring = " OR ".join(fqstring.split())
-        #         search_params['fq'] = fqstring
-        # except KeyError:
-        #     pass
+        fq = mk_field_queries(search_params)
+        print("OUTPUT search_params {}".format(str(fq)))
         return(search_params)
+
     
