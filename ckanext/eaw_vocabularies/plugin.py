93fcb1012a4babd367bbe75fed7d345bc6fa7187
Harald von Waldow <harald.vonwaldow@eawag.ch>
2016-02-03 21:25:26 +0100

    removed IDatasetform, moved schema stuff to ckanext-eaw_schema

--- ckanext/eaw_vocabularies/plugin.py
+++ ckanext/eaw_vocabularies/plugin.py
@@ -165,7 +165,6 @@ def mk_field_queries(search_params, vocabfields):
             
 class Eaw_VocabulariesPlugin(p.SingletonPlugin, tk.DefaultDatasetForm):
     p.implements(p.IConfigurer)
-    p.implements(p.IDatasetForm)
     p.implements(p.ITemplateHelpers)
     p.implements(p.IPackageController, inherit=True)
     
@@ -177,50 +176,6 @@ class Eaw_VocabulariesPlugin(p.SingletonPlugin, tk.DefaultDatasetForm):
         tk.add_template_directory(config_, 'templates')
         #tk.add_public_directory(config_, 'public')
         tk.add_resource('fanstatic', 'eaw_vocabularies')
-    # IDatasetform
-    def _modify_package_schema(self, schema):
-        schema.update({
-            'systems': [tk.get_validator('not_missing'),
-                       tk.get_converter('convert_to_tags')('systems')],
-            'variables': [tk.get_validator('not_missing'),
-                          tk.get_converter('convert_to_tags')('variables')],
-            'timerange': [tk.get_validator('not_missing'),
-                          tk.get_converter('convert_to_extras')]
-        })
-        return(schema)
-        
-    def create_package_schema(self):
-        schema = super(Eaw_VocabulariesPlugin, self).create_package_schema()
-        schema = self._modify_package_schema(schema)
-        return(schema)
-
-    def update_package_schema(self):
-        schema = super(Eaw_VocabulariesPlugin, self).update_package_schema()
-        schema = self._modify_package_schema(schema)
-        return(schema)
-
-    def show_package_schema(self):
-        schema = super(Eaw_VocabulariesPlugin, self).show_package_schema()
-        schema['tags']['__extras'].append(tk.get_converter('free_tags_only'))
-        schema.update({
-            'systems': [tk.get_converter('convert_from_tags')('systems'),
-                       tk.get_validator('ignore_missing')],
-            'variables': [tk.get_converter('convert_from_tags')('variables'),
-                          tk.get_validator('ignore_missing')],
-            'timerange': [tk.get_converter('convert_from_extras'),
-                          tk.get_validator('ignore_missing')]
-        })
-        return(schema)
-    
-    def is_fallback(self):
-        # Return True to register this plugin as the default handler for
-        # package types not handled by any other IDatasetForm plugin.
-        return True
-
-    def package_types(self):
-        # This plugin doesn't handle any special package types, it just
-        # registers itself as the default (above).
-        return []
     
     # ITemplateHelpers
     def get_helpers(self):
